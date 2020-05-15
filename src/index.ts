6694670b151e8e18c2bcce55ff274a5d7d1565ed
cranpun <cranpun@gmail.com>
2020-05-15 17:27:49 +0900

    bt-start

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { Camera } from "./camera";
 window.addEventListener("load", async () => {
     const c = new Camera();
     const btstart = document.querySelector("#bt-start");
-    btstart.addEventListener("click",async () => {
+    btstart.addEventListener("click",() => {
         const canv: HTMLCanvasElement = document.querySelector("#poseneckcanvas");
-        await c.start(canv);
+        c.start(canv);
     });
 });
